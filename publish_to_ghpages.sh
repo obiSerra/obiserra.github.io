df234af9e5249383a492cf09bbb64d6e3e5e9d3c
Roberto Serra <obi.serra@gmail.com>
2019-06-21 12:42:19 +0200

    publish script update

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -47,10 +47,10 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+hugo -t vec
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
